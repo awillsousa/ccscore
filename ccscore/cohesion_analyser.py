7586a92921d8a4b118e301b7457424d283ae699c
willian <antoniosousa@mpf.mp.br>
2020-12-03 21:40:45 -0300

    Adicionados arquivos pacote infernal

--- ccscore/cohesion_analyser.py
+++ ccscore/cohesion_analyser.py
@@ -1,22 +1,34 @@
 import spacy
-from document import Document
+from infernal import feature_extraction as fe
 
-nlp_process = spacy.load("")
+try:
+    nlp_process = spacy.load("pt_core_news_lg")
+except:
+    print("Erro ao carregar modelo spacy")
+
 
 # Cohesion analyzer class
 class CohesionAnalyzer(object):
 
-    def __init__(self):
-        self.funcoes_pipeline = [self.]
+    def __init__(self, doc):
+        self.funcoes_pipeline = [self.anotate_pos,
+                                    ]
         self.results = []
 
-    def analyze(self, doc):
-        self.results =  [f(doc) for f in self.funcoes_pipeline]
+        self.textdoc = doc
+        self.nlpdoc = None
+
+
+    def anotate_pos(self):
+        self.nlpdoc = nlp_process(self.textdoc)
+
+    def analyze(self):
+        self.results =  [f(self.textdoc) for f in self.funcoes_pipeline]
         
-    def analyze_local_cohesion(self, doc):
+    def analyze_local_cohesion(self):
         pass
 
-    def analyze_global_cohesion(self, doc):
+    def analyze_global_cohesion(self):
         pass
 
     
